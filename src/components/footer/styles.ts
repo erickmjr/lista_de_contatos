22f57393f06c921176982dbfe544d67ed6407e04
erickmjr <erickmellojr@gmail.com>
2025-06-17 23:57:44 -0300

    correcao fixed do footer

--- src/components/footer/styles.ts
+++ src/components/footer/styles.ts
@@ -3,10 +3,12 @@ import variaveis from '../../styles/variaveis';
 
 export const FooterStyled = styled.footer`
     width: 100%;
-    position: absolute;
+    position: fixed;
     bottom: 0;
+    left: 0;
     padding: 2.25dvh;
     color: ${variaveis.corTexto};
     background-color: ${variaveis.black1};
     text-align: center;
+    z-index: 100;
 `;
