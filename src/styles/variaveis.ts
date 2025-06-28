913d522ca0870724eb7db3133ccae9dcd6f08f56
erickmjr <erickmellojr@gmail.com>
2025-06-28 17:51:52 -0300

    add: verde

--- src/styles/variaveis.ts
+++ src/styles/variaveis.ts
@@ -5,5 +5,6 @@ export default {
     black1: '#000',
     corH1: '#edf2f4',
     vermelho1: '#b7094c',
+    verde1: '#40916c',
 };
 /* https://coolors.co/palette/10002b-240046-3c096c-5a189a-7b2cbf-9d4edd-c77dff-e0aaff */
